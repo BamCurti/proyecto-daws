b4178b2282b7d5c1ba486180fc0ff2fe1a2aec62
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2021-05-16 13:14:53 -0500

    API lista, estaticos definidos

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Shop Homepage v4.3.0 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+let carrito = [];
+let total = 0;
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const miLocalStorage = window.localStorage;
--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.use(cors({origin: ['http://127.0.0.1:5000']}));
 app.use(express.json());
 
 app.use(router);
-app.use(express.static(__dirname));
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/Home.html");
+})
+
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`);
