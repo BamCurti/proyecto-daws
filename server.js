44da331b47aa90ce39a72d879fb6bb236857c333
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2021-05-16 12:28:29 -0500

    Server static home

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({origin: ['http://127.0.0.1:5000']}));
 app.use(express.json());
 
 app.use(router);
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`);
